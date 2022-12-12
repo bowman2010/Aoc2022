ea7b6e623cf4683b4d9531219ee1d1892b924e7d
Fvanderpo <francois.vanderpoorte@gmail.com>
2022-12-12 13:17:27 +0100

    step back with puzzledir

--- Utils/aoc2022.h
+++ Utils/aoc2022.h
@@ -6,12 +6,8 @@
 #define PUZZLES_PATH "/home/bowman/Dev/Cpp_Projects/Aoc2022/Puzzles/"
 
 std::string puzzlePath(std::string fname) {
-    if(const char* ve = std::getenv("AOC2022_PPATH"))
-    {
-        std::string ppath(ve);
+        std::string ppath(PUZZLES_PATH);
         ppath.append("/"+fname);
         return ppath;
-    }
-    else exit(2);
 }
 #endif // AOC2022_H
